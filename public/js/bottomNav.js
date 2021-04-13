a0256c5b3138c5c46d4211cb456371089e049e43
Xuehan Cheng <xuehan868@gmail.com>
2021-04-12 21:18:38 -0700

    add bottom navbar (#68)
    Co-authored-by: azhou2 <zhou.andy98@gmail.com>
    Co-authored-by: xiaoyanfeifei <yanfeipgmm@gmail.com>
    Co-authored-by: uopsdod <uopsdod@gmail.com>
    Co-authored-by: xuehan100 <xuehan868@gmail.com>
    Co-authored-by: HungWenChiu <gmmefg411007@gmail.com>
    
    Co-authored-by: azhou2 <zhou.andy98@gmail.com>
    Co-authored-by: xiaoyanfeifei <yanfeipgmm@gmail.com>
    Co-authored-by: uopsdod <uopsdod@gmail.com>
    Co-authored-by: HungWenChiu <gmmefg411007@gmail.com>

---
+++ public/js/bottomNav.js
@@ -0,0 +1,26 @@
+$(document).ready(() => {
+  $("#bottom-nav-template").append(getBottomNavbar());
+  executeQuiz();
+});
+
+function executeQuiz() {
+  
+}
+
+/** Insert View */
+function getBottomNavbar() {
+  return `      
+        <nav class="bottom-navbar navbar-dark">
+            <div class="mr-auto d-inline-flex">
+        
+                <a class="nav-link nav-txt" href="/quiz" id="quiz-btn"><em class="fas fa-question-circle" aria-hidden="true"></em></a>
+                <a class="nav-link nav-txt" href="/shelter" id="shelter-btn"><em class="fas fa-home" aria-hidden="true"></em></a>
+                <a class="nav-link nav-txt" href="/emergencyContact" id="emergencyContact-btn"><em class="far fa-address-book" aria-hidden="true"></em></a>
+                <!-- Emergency Button needs to be centered-->
+                <a class="nav-link nav-txt" href="/emergencyButton" id="emergency-btn"><em class="fas fa-exclamation-circle" aria-hidden="true"></em></a>
+                <a class="nav-link nav-txt" href="/activity" id="activity-btn"><em class="fa fa-hands-helping" aria-hidden="true"></em></a>
+                <a class="nav-link nav-txt" href="#" id="smap-icon"><em class="fas fa-map-marked-alt" aria-hidden="true"></em></a>
+            </div>
+        </nav> `;
+}
+
