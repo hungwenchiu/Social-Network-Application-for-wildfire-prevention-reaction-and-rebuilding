679ce21639047665e3a55ec12f76e72c2abc0de6
Hung-Wen (Ted) Chiu <64296962+HungWenChiu@users.noreply.github.com>
2021-04-14 16:37:55 -0700

    Refactor (#69)
    * add bottom navbar
    
    Co-authored-by: azhou2 <zhou.andy98@gmail.com>
    Co-authored-by: xiaoyanfeifei <yanfeipgmm@gmail.com>
    Co-authored-by: uopsdod <uopsdod@gmail.com>
    Co-authored-by: xuehan100 <xuehan868@gmail.com>
    Co-authored-by: HungWenChiu <gmmefg411007@gmail.com>
    
    * fine-tune the bottom bar
    
    Co-authored-by: HungWenChiu <gmmefg411007@gmail.com>
    
    Co-authored-by: xuehan <xuehan868@gmail.com>
    Co-authored-by: azhou2 <zhou.andy98@gmail.com>
    Co-authored-by: xiaoyanfeifei <yanfeipgmm@gmail.com>
    Co-authored-by: uopsdod <uopsdod@gmail.com>

--- public/js/bottomNav.js
+++ public/js/bottomNav.js
@@ -10,7 +10,7 @@ function executeQuiz() {
 /** Insert View */
 function getBottomNavbar() {
   return `      
-        <nav class="bottom-navbar navbar-dark">
+        <nav class="navbar navbar-dark">
             <div class="mr-auto d-inline-flex">
         
                 <a class="nav-link nav-txt" href="/quiz" id="quiz-btn"><em class="fas fa-question-circle" aria-hidden="true"></em></a>
@@ -20,6 +20,7 @@ function getBottomNavbar() {
                 <a class="nav-link nav-txt" href="/emergencyButton" id="emergency-btn"><em class="fas fa-exclamation-circle" aria-hidden="true"></em></a>
                 <a class="nav-link nav-txt" href="/activity" id="activity-btn"><em class="fa fa-hands-helping" aria-hidden="true"></em></a>
                 <a class="nav-link nav-txt" href="#" id="smap-icon"><em class="fas fa-map-marked-alt" aria-hidden="true"></em></a>
+                <a class="nav-link nav-txt" href="#" id="smap-icon"><em class="fa fa-book" aria-hidden="true"></em></a>
             </div>
         </nav> `;
 }
